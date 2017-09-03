b23e20a33adf35a08dbfe2edc4aa726cfe08031b
vpaliy <vpaliy97@gmail.com>
2017-09-02 23:53:37 -0400

    clean project

---
+++ app/src/main/java/com/vpaliy/melophile/di/Config.java
@@ -0,0 +1,8 @@
+package com.vpaliy.melophile.di;
+
+
+public final class Config {
+    public static final String CLIENT_ID="INSERT_YOUR_CLIENT_ID";
+    public static final String CLIENT_SECRET_ID="INSERT_YOUR_SERCRET_ID";
+    public static final String REDIRECT_URI="INSERT_YOUR_REDIRECT_URI";
+}
---
+++ data/src/main/java/com/vpaliy/data/Config.java
@@ -0,0 +1,6 @@
+package com.vpaliy.data;
+
+
+public final class Config {
+    public static final String CLIENT_ID="INSERT_YOUR_KEY";
+}
